3c8f7830c7445b383b5844e42ce45bc3ba12a1b1
Amrit Manash <amritmanash9@gmail.com>
2022-04-02 19:55:09 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class sum
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int result=a+b;
+		System.out.println("The summation of two number is "+result);
+	}
+}
